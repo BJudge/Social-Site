c003a67da0bba0c77042f8abee1c67aa4da569b3
BJudge <bartjudge@gmail.com>
2021-11-26 18:49:15 +0000

    infinite scrolling

--- index.php
+++ index.php
@@ -28,11 +28,118 @@ if(isset($_POST['post'])){
                         <input type="submit" name="post" id="post_button" value="Post">
                         <hr>
                     </form>
-                   <?php $post = new Post($con, $userLoggedIn);
-                   $post->loadPostsFriends();
-                   ?>
 
+                   <div class="posts_area"></div>
+                   <img src="./assets/images/icons/loading.gif" alt="Loading Gif" id="loading">
+                
+                       <!-- gif should be placed here -->
             </div>
+            <script>
+                $(function(){
+ 
+                    var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+                    var inProgress = false;
+
+                    loadPosts(); //Load first posts
+
+                    $(window).scroll(function() {
+                        var bottomElement = $(".status_post").last();
+                        var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+
+                        // isElementInViewport uses getBoundingClientRect(), which requires the HTML DOM object, not the jQuery object. The jQuery equivalent is using [0] as shown below.
+                        if (isElementInView(bottomElement[0]) && noMorePosts == 'false') {
+                            loadPosts();
+                        }
+                    });
+
+                    function loadPosts() {
+                        if(inProgress) { //If it is already in the process of loading some posts, just return
+                            return;
+                        }
+                        
+                        inProgress = true;
+                        $('#loading').show();
+
+                        var page = $('.posts_area').find('.nextPage').val() || 1; //If .nextPage couldn't be found, it must not be on the page yet (it must be the first time loading posts), so use the value '1'
+
+                        $.ajax({
+                            url: "includes/handlers/ajax_load_posts.php",
+                            type: "POST",
+                            data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+                            cache:false,
+
+                            success: function(response) {
+                                $('.posts_area').find('.nextPage').remove(); //Removes current .nextpage 
+                                $('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage 
+                                $('.posts_area').find('.noMorePostsText').remove(); //Removes current .nextpage 
+
+                                $('#loading').hide();
+                                $(".posts_area").append(response);
+
+                                inProgress = false;
+                            }
+                        });
+                    }
+
+                        //Check if the element is in view
+                        function isElementInView (el) {
+                            var rect = el.getBoundingClientRect();
+
+                            return (
+                                rect.top >= 0 &&
+                                rect.left >= 0 &&
+                                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && //* or $(window).height()
+                                rect.right <= (window.innerWidth || document.documentElement.clientWidth) //* or $(window).width()
+                            );
+                        }
+                        });
+
+                // var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+                
+                // $(document).ready(function(){
+                //     $('#loading').show();
+                    
+                //     $.ajax({
+                //         url: "./includes/handlers/ajax_load_posts.php",
+                //         type:"POST",
+                //         data: "page=1&userLoggedIn=" + userLoggedIn,
+                //         cache: false,
+                //         success: function(data){
+                //             $('#loading').hide();
+                //             $('.posts_area').html(data);
+                //         }
+                //     })
+                    
+                //     $(window).scroll(function(){
+                //         var height = $('.posts_area').height();
+                //         var scroll_top = $(this).scrollTop();
+                //         var page = $('.posts_area').find('.nextPage').val();
+                //         var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+
+                //         console.log('hello from the scroll callback');
+
+                        
+                //         if((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
+                //             $('#loading'.show());
+
+                //             var ajaxReq =  $.ajax({
+                //                 url: "./includes/handlers/ajax_load_posts.php",
+                //                 type:"POST",
+                //                 data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+                //                 cache: false,
+                //                 success: function(response){
+                //                     $('.posts_area').find('.nextPage').remove();
+                //                     $('.posts_area').find('.noMorePosts').remove();
+                //                     $('#loading').hide();
+                //                     $('.posts_area').append(response);
+                //                 }
+                //             })
+                //         }
+                //             return false;
+                    
+                //     });
+                // })
+            </script>
         </div>   
     </body>
 </html>
