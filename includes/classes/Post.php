3ffafd574ccd2531fe06457d9a2c8854ba58dbc9
BJudge <bartjudge@gmail.com>
2021-12-01 23:37:21 +0000

    adding like functionaity

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -183,6 +183,8 @@ class Post {
                                 </div>
                                 <div class='newsfeedPostedOptions'>
                                 Comments($comments_check_num)&nbsp;&nbsp;&nbsp;
+                                <iframe src='like.php?post_id=$id' scrolling='no'>
+                                </iframe>
                                 </div>
                             </div>
                             <div class='post_comment' id='toggleComment$id' style='display:none;'>
--- profile.php
+++ profile.php
@@ -1,5 +1,9 @@
 <?php 
 include('./includes/header.php');
+
+if(isset($_GET['profile_username'])){
+    $username = $_GET['profile_username'];
+}
 ?>
             <div class="main_column column">
                     This is a profile page
