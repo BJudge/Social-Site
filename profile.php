c1bbad098cd1abf3d10d455979b399830ac6559c
BJudge <bartjudge@gmail.com>
2021-12-05 09:25:25 +0000

    adding friend function and removing friend function

--- profile.php
+++ profile.php
@@ -9,6 +9,21 @@ if(isset($_GET['profile_username'])){
     $user_array = mysqli_fetch_array($user_details_query);
     $num_friends = (substr_count($user_array['friend_array'], ",")) - 1;
 }
+
+if(isset($_POST['remove_friend'])){
+    $user = new USER($con, $userLoggedIn);
+    $user->removeFriend($username);
+}
+
+if(isset($_POST['add_friend'])){
+    $user = new USER($con, $userLoggedIn);
+    $user->sendRequest($username);
+}
+
+if(isset($_POST['respond_request'])){
+    header("Location: request.php");
+}
+
 ?>
         <style>
             .wrapper {
@@ -23,7 +38,7 @@ if(isset($_GET['profile_username'])){
                     <p><?php echo "Posts: " . $user_array['num_posts'] ?></p>
                     <p><?php echo "Likes: " . $user_array['num_likes']; ?></p> 
                     <p><?php echo "Friends: " . $num_friends ?></p> 
-                    <form action="<?php echo $username;?>">
+                    <form action="<?php echo $username;?>" method="POST">
                         <?php 
                             $profile_user_obj= new User($con, $username);
                             if($profile_user_obj->isClosed()){
@@ -34,6 +49,12 @@ if(isset($_GET['profile_username'])){
                             if($userLoggedIn != $username) {
                                 if($logged_in_user_obj->isFriend($username)){
                                     echo "<input type='submit' name='remove_friend' class='danger' value= 'Remove Friend'> </br>";
+                                } else if($logged_in_user_obj->didRecieveRequest($username)){
+                                    echo "<input type='submit' name='respond_request' class='warning' value= 'Respond To Request'> </br>";
+                                } else if($logged_in_user_obj->didSendRequest($username)){
+                                    echo "<input type='submit' name='' class='default' value= 'Request Sent'> </br>";
+                                } else {
+                                    echo "<input type='submit' name='add_friend' class='success' value= 'Add Friend'> </br>";
                                 }
                             }
                         ?>       
